25742929544259070fed588564a899d12c68fa50
willy <willybalt.2017@gmail.com>
2025-07-13 14:39:11 -0400

    se agrego los ervicios del crud

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,10 @@
+// servicio que conecta con la base de datos 
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    async onModuleInit() {
+        await this.$connect();
+    }
+}
